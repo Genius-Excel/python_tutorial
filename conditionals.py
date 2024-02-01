ae587100db375a640934f0680ef19060dfe89d56
Genius-Excel <geniusexcel9@gmail.com>
2024-02-01 17:05:28 +0100

    This file contains the basic flow explanation of a conditional statement in Python.

---
+++ conditionals.py
@@ -0,0 +1,41 @@
+# Conditionals are used to define to determine a program
+
+# single equality sign is used to assign a value to a variable.
+
+# Double equality sign is used to compare or chek for equality.
+
+number = 100
+
+print(type(number))
+
+string = '100'
+print(type(string))
+
+print(number is string)
+
+
+
+# if number == '100':
+#     print("The number is 100.")
+#     print("Hello yes the condition was true.")
+# elif number == 10098:
+#     print("The second condition worked.")
+# elif number == 574838:
+#     print("This is the thrid condtion that is true,")
+# else:
+#     print("None of the values is equal to 100.")
+
+
+
+# day = "suuny"
+
+# if day == "sunny":
+#     print("the day is sunny, chill at home.")
+# elif day == "rainny":
+#     print("It is rainnig go with your umbrella.")
+# elif day == "snowy":
+#     print("It is snowing, go with your coat.")
+# else:
+#     print("The day is cool, get some fresh air.")
+
+
