eb45657b4b822c6ee3d1bf51a8885ae83fa46da6
Genius-Excel <geniusexcel9@gmail.com>
2024-02-01 17:04:15 +0100

    This file contains explanation of the 'print()' function and using it to print 'Hello World'.

---
+++ hello.py
@@ -0,0 +1,27 @@
+# I am saying 'Hello World' with a varible.
+
+# name = "Favour"
+
+# print(name)
+
+# print("Favour")
+
+hello = "Good day"
+
+hello = "Good night"
+
+number = 10
+
+yes = True 
+
+no = False
+
+print(hello) # ---- Good night
+
+print(number) # ---- 10
+
+print(yes) # ----- True 
+
+print(no) # ----- False
+
+# Priniciples -- DRY (DO NOT REPEAT YOURSELF)
